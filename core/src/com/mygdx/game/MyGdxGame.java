203c58ff15ad23c210169f4c013d6dd757e40ec9
MrDengazen <denchiksrty@gmail.com>
2023-07-05 17:23:00 +0400

    egg player added fix

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -28,10 +28,10 @@ public class MyGdxGame extends ApplicationAdapter {
         batch = new SpriteBatch();
         terrain = new Texture("Terrain.PNG");
         sky = new Texture("Sky.jpg");
-        player = new Player(new Texture("egg.png"), 0, 0 , 600,
-                512, 150,128, 0,0,0,0);
         SCR_WIDTH = Gdx.graphics.getWidth();
         SCR_HEIGHT = Gdx.graphics.getHeight();
+        player = new Player(new Texture("egg.png"), 0, 0 , 600,
+                512, SCR_WIDTH / 9,SCR_HEIGHT / 5, 0,0,0,0);
         gwidth = SCR_WIDTH / 9;
         gheight = SCR_HEIGHT / 9.6f;
         camera = new OrthographicCamera();
--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -6,13 +6,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 public class Player extends Sprite {
     float x, y;
     float vx, vy;
-    float width, height;
 
     Player(Texture img, int srcx, int srcy, int srcwidth, int srcheight, float width, float height,
            float x, float y, float vx, float vy) {
         super(img, srcx, srcy, srcwidth, srcheight);
-        this.width = width;
-        this.height = height;
         this.x = x;
         this.y = y;
         this.vx = vx;
