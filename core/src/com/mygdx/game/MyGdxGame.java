5396c4e986e9a207d1967ea75af1136132a8ceb5
theodoks <theodoks@yandex.ru>
2023-07-05 13:44:07 +0300

    initF
    (cherry picked from commit 3c5001b1638fece0a2b82b729f800decf2462811)

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -17,6 +17,7 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		//comment
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
