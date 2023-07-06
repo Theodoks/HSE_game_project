8311837c05669c69015b36f2c94b3bd7d6d300a2
MrDengazen <denchiksrty@gmail.com>
2023-07-06 16:23:53 +0400

    egg player collide and move

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -3,18 +3,61 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Player extends Sprite {
     float x, y;
     float vx, vy;
+    boolean onGround, bodyRotation = true;
+    float moveSpeed, powerJump, gravity;
 
-    Player(Texture img, int srcx, int srcy, int srcwidth, int srcheight, float width, float height,
-           float x, float y, float vx, float vy) {
-        super(img, srcx, srcy, srcwidth, srcheight);
+    Player(Texture img, float width, float height, float x, float y, float moveSpeed, float powerJump, float gravity) {
+        super(img, 0, 0, img.getWidth(), img.getHeight());
         this.x = x;
         this.y = y;
-        this.vx = vx;
-        this.vy = vy;
+        this.vx = 0;
+        this.vy = 0;
+        this.gravity = gravity;
         setSize(width, height);
         setPosition(x, y);
     }
+    void update(boolean right, boolean left, boolean up, ArrayList<Object> objects){
+        x += vx;
+        y += vy;
+        setPosition(x, y);
+        collide(vx, vy, objects);
+        if(up){
+            if(onGround){
+                vy = -powerJump;
+            }
+        }
+        if(left){
+            vx = -moveSpeed;
+            if(bodyRotation){
+                flip(true, false);
+                bodyRotation = false;
+            }
+        }
+        if(right){
+            vx = moveSpeed;
+            if (!bodyRotation){
+                flip(true, false);
+                bodyRotation = true;
+            }
+        }
+        if(!right && !left){
+            vx = 0;
+        }
+        if(!onGround){
+            vy += gravity;
+        }
+        onGround = false;
+    }
+        void collide(float vx, float vy, ArrayList<Object> objects)
+        {
+            for(int i = 0; i < objects.size(); i++){
+                
+            }
+        }
 }
